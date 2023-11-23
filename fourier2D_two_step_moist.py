b961991e8b600de72d590f2266718098410d442d
lupin <lupified@gmail.com>
2023-11-23 10:33:00 +0000

    modifications to FNO2d class; slight modifications to loss to include multiple channels

--- fourier2D_two_step_moist.py
+++ fourier2D_two_step_moist.py
@@ -99,7 +99,6 @@ class FNO2d(nn.Module):
         self.modes2 = modes2
         self.width = width
         self.padding = 8 # pad the domain if input is non-periodic
-
         self.p = nn.Linear(channels+2, self.width) # input channel is 12: the solution of the previous 10 timesteps + 2 locations (u(t-10, x, y), ..., u(t-1, x, y),  x, y)
         self.conv0 = SpectralConv2d(self.width, self.width, self.modes1, self.modes2)
         self.conv1 = SpectralConv2d(self.width, self.width, self.modes1, self.modes2)
@@ -122,6 +121,94 @@ class FNO2d(nn.Module):
         grid = self.get_grid(x.shape, x.device)
         #grid = get_grid(x.shape, x.device)
         x = torch.cat((x, grid), dim=-1)
+        x = self.p(x)
+        #x = F.pad(x, [0,self.padding, 0, 0]) # pad the domain if input is non-periodic
+        x = x.permute(0, 3, 1, 2)
+        
+        x1 = self.norm(self.conv0(self.norm(x)))
+        x1 = self.mlp0(x1)
+        x2 = self.w0(x)
+        x = x1 + x2
+        x = F.gelu(x)
+
+        x1 = self.norm(self.conv1(self.norm(x)))
+        x1 = self.mlp1(x1)
+        x2 = self.w1(x)
+        x = x1 + x2
+        x = F.gelu(x)
+
+        x1 = self.norm(self.conv2(self.norm(x)))
+        x1 = self.mlp2(x1)
+        x2 = self.w2(x)
+        x = x1 + x2
+        x = F.gelu(x)
+
+        x1 = self.norm(self.conv3(self.norm(x)))
+        x1 = self.mlp3(x1)
+        x2 = self.w3(x)
+        x = x1 + x2
+        #print(x.shape)
+        x = self.q(x) # why does the resulting shape have the same?
+        #print(x.shape)
+        x = x.permute(0, 2, 3, 1)
+        #x = x[:, :-self.padding, :, :] # pad the domain if input is non-periodic
+        return x
+
+    def get_grid(self, shape, device):
+        # normalized from 0 to 1?
+        ## this is a pretty strange way to implement for grid free prediction
+        batchsize, size_x, size_y = shape[0], shape[1], shape[2]
+        gridx = torch.tensor(np.linspace(0, 1, size_x), dtype=torch.float)
+        gridx = gridx.reshape(1, size_x, 1, 1).repeat([batchsize, 1, size_y, 1])
+        gridy = torch.tensor(np.linspace(0, 1, size_y), dtype=torch.float)
+        gridy = gridy.reshape(1, 1, size_y, 1).repeat([batchsize, size_x, 1, 1])
+        return torch.cat((gridx, gridy), dim=-1).to(device)
+
+class FNO2d_mod1(nn.Module):
+    def __init__(self, modes1, modes2, width, padding = 8, channels = 3, channelsout = 3):
+        super(FNO2d, self).__init__()
+
+        """
+        The overall network. It contains 4 layers of the Fourier layer.
+        1. Lift the input to the desire channel dimension by self.fc0 .
+        2. 4 layers of the integral operators u' = (W + K)(u).
+            W defined by self.w; K defined by self.conv .
+        3. Project from the channel space to the output space by self.fc1 and self.fc2 .
+        
+        input: the solution of the previous 10 timesteps + 2 locations (u(t-10, x, y), ..., u(t-1, x, y),  x, y)
+        input shape: (batchsize, x=64, y=64, c=12)
+        output: the solution of the next timestep
+        output shape: (batchsize, x=64, y=64, c=1)
+        """
+
+        self.modes1 = modes1
+        self.modes2 = modes2
+        self.width = width
+        self.padding = 8 # pad the domain if input is non-periodic
+        self.c = nn.Conv2d(channels+2, channels+2, kernel_size = Union[int, Tuple[int, int, int]],)
+        self.p = nn.Linear(channels+2, self.width) # input channel is 12: the solution of the previous 10 timesteps + 2 locations (u(t-10, x, y), ..., u(t-1, x, y),  x, y)
+        self.conv0 = SpectralConv2d(self.width, self.width, self.modes1, self.modes2)
+        self.conv1 = SpectralConv2d(self.width, self.width, self.modes1, self.modes2)
+        self.conv2 = SpectralConv2d(self.width, self.width, self.modes1, self.modes2)
+        self.conv3 = SpectralConv2d(self.width, self.width, self.modes1, self.modes2)
+        self.mlp0 = MLP(self.width, self.width, self.width)
+        self.mlp1 = MLP(self.width, self.width, self.width)
+        self.mlp2 = MLP(self.width, self.width, self.width)
+        self.mlp3 = MLP(self.width, self.width, self.width)
+        self.w0 = nn.Conv2d(self.width, self.width, 1)
+        self.w1 = nn.Conv2d(self.width, self.width, 1)
+        self.w2 = nn.Conv2d(self.width, self.width, 1)
+        self.w3 = nn.Conv2d(self.width, self.width, 1)
+        self.norm = nn.InstanceNorm2d(self.width)
+        self.q = MLP(self.width, channelsout, self.width * 4) # output channel is 1: u(x, y)
+        
+## this is not working....bring back the grid addition in this step if cant fix...done
+    def forward(self, x):
+        # adds grid coordinates to the end of the input array for FNO prediction x,y coordinates
+        grid = self.get_grid(x.shape, x.device)
+        #grid = get_grid(x.shape, x.device)
+        x = torch.cat((x, grid), dim=-1)
+        x = self.c(x)
         x = self.p(x)
         #x = F.pad(x, [0,self.padding, 0, 0]) # pad the domain if input is non-periodic
         x = x.permute(0, 3, 1, 2)
@@ -164,15 +251,6 @@ class FNO2d(nn.Module):
         gridy = torch.tensor(np.linspace(0, 1, size_y), dtype=torch.float)
         gridy = gridy.reshape(1, 1, size_y, 1).repeat([batchsize, size_x, 1, 1])
         return torch.cat((gridx, gridy), dim=-1).to(device)
-
-# def get_grid(shape, device):
-    # # normalized from 0 to 1?
-    # batchsize, size_x, size_y = shape[0], shape[1], shape[2]
-    # gridx = torch.tensor(np.linspace(0, 1, size_x), dtype=torch.float)
-    # gridx = gridx.reshape(1, size_x, 1, 1).repeat([batchsize, 1, size_y, 1])
-    # gridy = torch.tensor(np.linspace(0, 1, size_y), dtype=torch.float)
-    # gridy = gridy.reshape(1, 1, size_y, 1).repeat([batchsize, size_x, 1, 1])
-    # return torch.cat((gridx, gridy), dim=-1).to(device)
 
 def regular_loss(output, target):
 
@@ -217,80 +295,7 @@ def estimate_anticyclone (output,target,mask):
 
     return loss
 
-# old
-# def spectral_loss(output, output2, target, target2, wavenum_init,wavenum_init_ydir,lamda_reg,ocean_grid):
-
-    # loss1 = torch.sum((output-target)**2)/ocean_grid + torch.sum((output2-target2)**2)/ocean_grid
-    # # loss1 = torch.abs((output-target))/ocean_grid
-
-    # out_fft = torch.mean(torch.abs(torch.fft.rfft(output[:,:,:,0],dim=2)),dim=1)
-    # target_fft = torch.mean(torch.abs(torch.fft.rfft(target[:,:,:,0],dim=2)),dim=1)
-
-    # out_fft_ydir = torch.mean(torch.abs(torch.fft.rfft(output[:,:,:,0],dim=1)),dim=2)
-    # target_fft_ydir = torch.mean(torch.abs(torch.fft.rfft(target[:,:,:,0],dim=1)),dim=2)
-
-    # out2_fft = torch.mean(torch.abs(torch.fft.rfft(output2[:,:,:,0],dim=2)),dim=1)
-    # target2_fft = torch.mean(torch.abs(torch.fft.rfft(target2[:,:,:,0],dim=2)),dim=1)
-
-    # out2_fft_ydir = torch.mean(torch.abs(torch.fft.rfft(output2[:,:,:,0],dim=1)),dim=2)
-    # target2_fft_ydir = torch.mean(torch.abs(torch.fft.rfft(target2[:,:,:,0],dim=1)),dim=2)
-
-
-    # # loss2 = torch.mean(torch.abs(out_fft[:,0:wavenum_init]-target_fft[:,0:wavenum_init]))
-    # # loss2_ydir = torch.mean(torch.abs(out_fft_ydir[:,0:wavenum_init_ydir]-target_fft_ydir[:,0:wavenum_init_ydir]))
-
-    # loss2 = torch.mean(torch.abs(out_fft[:,wavenum_init:]-target_fft[:,wavenum_init:]))
-    # loss2_ydir = torch.mean(torch.abs(out_fft_ydir[:,wavenum_init_ydir:]-target_fft_ydir[:,wavenum_init_ydir:]))
-
-    # # LC_loss_cyclone = estimate_cyclone (output,target,mask_rho_torch)
-    # # LC_loss_anticyclone = estimate_anticyclone (output,target,mask_rho_torch)
-
-    # loss2_next_time = torch.mean(torch.abs(out2_fft[:,wavenum_init:]-target2_fft[:,wavenum_init:]))
-    # loss2_ydir_next_time = torch.mean(torch.abs(out2_fft_ydir[:,wavenum_init_ydir:]-target2_fft_ydir[:,wavenum_init_ydir:]))
-
-
-    # # loss = (1-lamda_reg)*loss1 + 0.33*lamda_reg*loss2 + 0.33*lamda_reg*loss2_ydir + 0.33*LC_loss
-    # loss = ((1-lamda_reg)*loss1 + 0.25*(lamda_reg)*loss2 + 0.25*(lamda_reg)*loss2_ydir + 0.25*(lamda_reg)*loss2_next_time+0.25*(lamda_reg)*loss2_ydir_next_time)
-
-    # return loss
-
-# def spectral_loss(output, output2, target, target2, wavenum_init,wavenum_init_ydir,lamda_reg,ocean_grid):
-
-    # loss1 = torch.sum((output-target)**2)/ocean_grid + torch.sum((output2-target2)**2)/ocean_grid
-    # # loss1 = torch.abs((output-target))/ocean_grid
-
-    # out_fft = torch.mean(torch.abs(torch.fft.rfft(output[:,:,:,0],dim=2)),dim=1)
-    # target_fft = torch.mean(torch.abs(torch.fft.rfft(target[:,:,:,0],dim=2)),dim=1)
-
-    # out_fft_ydir = torch.mean(torch.abs(torch.fft.rfft(output[:,:,:,0],dim=1)),dim=2)
-    # target_fft_ydir = torch.mean(torch.abs(torch.fft.rfft(target[:,:,:,0],dim=1)),dim=2)
-
-    # out2_fft = torch.mean(torch.abs(torch.fft.rfft(output2[:,:,:,0],dim=2)),dim=1)
-    # target2_fft = torch.mean(torch.abs(torch.fft.rfft(target2[:,:,:,0],dim=2)),dim=1)
-
-    # out2_fft_ydir = torch.mean(torch.abs(torch.fft.rfft(output2[:,:,:,0],dim=1)),dim=2)
-    # target2_fft_ydir = torch.mean(torch.abs(torch.fft.rfft(target2[:,:,:,0],dim=1)),dim=2)
-
-
-    # # loss2 = torch.mean(torch.abs(out_fft[:,0:wavenum_init]-target_fft[:,0:wavenum_init]))
-    # # loss2_ydir = torch.mean(torch.abs(out_fft_ydir[:,0:wavenum_init_ydir]-target_fft_ydir[:,0:wavenum_init_ydir]))
-
-    # loss2 = torch.mean(torch.abs(out_fft[:,wavenum_init:]-target_fft[:,wavenum_init:]))
-    # loss2_ydir = torch.mean(torch.abs(out_fft_ydir[:,wavenum_init_ydir:]-target_fft_ydir[:,wavenum_init_ydir:]))
-
-    # # LC_loss_cyclone = estimate_cyclone (output,target,mask_rho_torch)
-    # # LC_loss_anticyclone = estimate_anticyclone (output,target,mask_rho_torch)
-
-    # loss2_next_time = torch.mean(torch.abs(out2_fft[:,wavenum_init:]-target2_fft[:,wavenum_init:]))
-    # loss2_ydir_next_time = torch.mean(torch.abs(out2_fft_ydir[:,wavenum_init_ydir:]-target2_fft_ydir[:,wavenum_init_ydir:]))
-
-
-    # # loss = (1-lamda_reg)*loss1 + 0.33*lamda_reg*loss2 + 0.33*lamda_reg*loss2_ydir + 0.33*LC_loss
-    # loss = ((1-lamda_reg)*loss1 + 0.25*(lamda_reg)*loss2 + 0.25*(lamda_reg)*loss2_ydir + 0.25*(lamda_reg)*loss2_next_time+0.25*(lamda_reg)*loss2_ydir_next_time)
-
-    # return loss
-
-def spectral_loss_channels(output, 
+def spectral_loss_channels_og(output, 
                            target, 
                            wavenum_init_lon, 
                            wavenum_init_lat, 
@@ -304,34 +309,96 @@ def spectral_loss_channels(output,
     # loss_grid = torch.sum((output-target)**2)/(grid_valid_size*channels)
     loss_grid = torch.mean((output-target)**2)
     # loss1 = torch.abs((output-tnparget))/ocean_grid
-
+ 
     run_loss_run = torch.zeros(1).float().cuda()
     
     for c in range(channels):
         ## losses from fft, both lat (index 1) and lon (index 2) directions
         ## ignore lat for now
         ## lat
-        #out_fft_lat = torch.mean(torch.abs(torch.fft.rfft(output[:,:,:,c],dim=1)),dim=2)
-        #target_fft_lat = torch.mean(torch.abs(torch.fft.rfft(target[:,:,:,c],dim=1)),dim=2)
-        #loss_fft_lat = torch.mean(torch.abs(out_fft_lat[:,wavenum_init_lat:]-target_fft_lat[:,wavenum_init_lat:]))
+        out_fft_lat = torch.mean(torch.abs(torch.fft.rfft(output[:,:,:,c],dim=1)),dim=2)
+        target_fft_lat = torch.mean(torch.abs(torch.fft.rfft(target[:,:,:,c],dim=1)),dim=2)
+        loss_fft_lat = torch.mean(torch.abs(out_fft_lat[:,wavenum_init_lat:]-target_fft_lat[:,wavenum_init_lat:]))
         ## lon
-        # out_fft_lon = torch.mean(torch.abs(torch.fft.rfft(output[:,:,:,c],dim=2)),dim=1)
-        # target_fft_lon = torch.mean(torch.abs(torch.fft.rfft(target[:,:,:,c],dim=2)),dim=1)
-        # loss_fft_lon = torch.mean(torch.abs(out_fft_lon[:,wavenum_init_lon:]-target_fft_lon[:,wavenum_init_lon:]))
+        out_fft_lon = torch.mean(torch.abs(torch.fft.rfft(output[:,:,:,c],dim=2)),dim=1)
+        target_fft_lon = torch.mean(torch.abs(torch.fft.rfft(target[:,:,:,c],dim=2)),dim=1)
+        loss_fft_lon = torch.mean(torch.abs(out_fft_lon[:,wavenum_init_lon:]-target_fft_lon[:,wavenum_init_lon:]))
         
-        ## it makes sense for me to take fft differences before...if you take mean, you lose important behavior at the equator?
-        out_fft_lon = torch.abs(torch.fft.rfft(output[:,:,:,c],dim=2))
-        target_fft_lon = torch.abs(torch.fft.rfft(target[:,:,:,c],dim=2))
-        loss_fft_lon = torch.mean((out_fft_lon - target_fft_lon)**2)
-
-        run_loss_run += loss_fft_lon
-        
-    loss_fft = run_loss_run/(channels)
+        run_loss_run += loss_fft_lon + loss_fft_lat
+    
+    ## fft_loss_scale included, so the lambda_fft is more intuitive (lambda_fft = .5 --> around half weighted shared between grid and fft loss)
+    loss_fft = run_loss_run/(2*channels*fft_loss_scale)
     loss_fft_weighted = lambda_fft*loss_fft
     loss_grid_weighted = ((1-lambda_fft))*loss_grid
     loss = loss_grid_weighted + loss_fft_weighted
     
     return loss, loss_grid, loss_fft
+
+def spectral_loss_channels_sqr(output, 
+                           target, 
+                           wavenum_init_lon, 
+                           wavenum_init_lat, 
+                           lambda_fft,
+                           grid_valid_size,
+                           channels = 3,
+                           fft_loss_scale = 1./110.):
+
+    # loss1 = torch.sum((output-target)**2)/ocean_grid + torch.sum((output2-target2)**2)/ocean_grid
+    
+    ## loss from grid space
+    # loss_grid = torch.sum((output-target)**2)/(grid_valid_size*channels)
+    loss_grid = torch.mean((output-target)**2)
+    # loss1 = torch.abs((output-tnparget))/ocean_grid
+
+    run_loss_run = torch.zeros(1).float().cuda()
+    
+    for c in range(channels):
+        ## it makes sense for me to take fft differences before...if you take mean, you lose more important differences at the equator?
+        out_fft_lat = torch.abs(torch.fft.rfft(output[:,:,:,c],dim=1))[:,wavenum_init_lon:,:]
+        target_fft_lat = torch.abs(torch.fft.rfft(target[:,:,:,c],dim=1))[:,wavenum_init_lon:,:]
+        loss_fft_lat = torch.mean((out_fft_lat - target_fft_lat)**2)
+        
+        out_fft_lon = torch.abs(torch.fft.rfft(output[:,:,:,c],dim=2))[:,:,wavenum_init_lon:]
+        target_fft_lon = torch.abs(torch.fft.rfft(target[:,:,:,c],dim=2))[:,:,wavenum_init_lon:]
+        loss_fft_lon = torch.mean((out_fft_lon - target_fft_lon)**2)
+
+        run_loss_run += loss_fft_lon + loss_fft_lat
+    
+    ## fft_loss_scale included, so the lambda_fft is more intuitive (lambda_fft = .5 --> around half weighted shared between grid and fft loss)
+    loss_fft = run_loss_run/(2*channels)*fft_loss_scale
+    loss_fft_weighted = lambda_fft*loss_fft
+    loss_grid_weighted = ((1-lambda_fft))*loss_grid
+    loss = loss_grid_weighted + loss_fft_weighted
+    
+    return loss, loss_grid, loss_fft
+
+# def RK4step(net,input_batch):
+    # output_1 = net(input_batch.cuda())
+    # output_2= net(input_batch.cuda()+0.5*output_1)
+    # output_3 = net(input_batch.cuda()+0.5*output_2)
+    # output_4 = net(input_batch.cuda()+output_3)
+
+    # return input_batch.cuda() + (output_1+2*output_2+2*output_3+output_4)/6
+    
+# delta_t = 1.0
+
+# def Eulerstep(net,input_batch):
+    # output_1 = net(input_batch.cuda())
+    # return input_batch.cuda() + delta_t*(output_1)
+
+
+# def PECstep(net,input_batch):
+    # output_net = net(input_batch.cuda())
+    # #assert output_net.shape == input_batch.shape 
+    # output_1 = output_net + input_batch.cuda()
+    # #print(net(input_batch.cuda()).shape, input_batch.cuda().shape)
+    # return input_batch.cuda() + delta_t*0.5*(net(input_batch.cuda())+net(output_1))
+
+# def directstep(net,input_batch):
+    # output_1 = net(input_batch.cuda())
+    # return output_1
+
+## these only work for single steps...
 
 def RK4step(net,input_batch):
     output_1 = net(input_batch.cuda())
@@ -350,14 +417,15 @@ def Eulerstep(net,input_batch):
 
 def PECstep(net,input_batch):
     output_net = net(input_batch.cuda())
-    assert output_net.shape == input_batch.shape 
+    #assert output_net.shape == input_batch.shape 
     output_1 = output_net + input_batch.cuda()
     #print(net(input_batch.cuda()).shape, input_batch.cuda().shape)
     return input_batch.cuda() + delta_t*0.5*(net(input_batch.cuda())+net(output_1))
 
 def directstep(net,input_batch):
     output_1 = net(input_batch.cuda())
-    return output_1
+    return output_1        
+
 
 def main():
     ################################################################
@@ -560,7 +628,7 @@ def main():
     psi_test_label_Tr = psi_test_label_Tr_torch_denorm.detach().cpu().numpy()
 
     M=100
-    autoreg_pred = np.zeros([M,1,Nlat,Nlon])
+    autoreg_pred = np.zeros([M,1,Nlat,Nlon]) 
 
     for k in range(0,M):
 
